93f4480a9d1c6142ab3cf332a0b657caff0c5673
tuan2203 <49948743+tuan2203@users.noreply.github.com>
2020-04-12 09:56:44 +0700

    doing2

--- js/main.js
+++ js/main.js
@@ -27,80 +27,80 @@ fromAPI(listAllDogURL)
         };
         index !== 0 && index % 4 === 0 ? counter++ : "";
 
-        let divNodeCol = document.createElement('div');
-        divNodeCol.className = 'col-sm border listAphabet';
-       
-        divNodeCol.innerHTML = item;
-        document.getElementsByClassName('rowList')[counter - 4].appendChild(divNodeCol);
+        let divNodeCol1 = document.createElement('div');
+        divNodeCol1.className = 'col-sm border listAphabet';
+
+        divNodeCol1.innerHTML = item;
+        document.getElementsByClassName('rowList')[counter - 4].appendChild(divNodeCol1); //let divNodeRow
 
         let divNodeRow2 = document.createElement('div');
         divNodeRow2.className = 'row';
-        divNodeCol.appendChild(divNodeRow2);
+        divNodeCol1.appendChild(divNodeRow2);
 
         let divNodeCo2 = document.createElement('div');
         divNodeCo2.className = 'col-sm border containName';
-        divNodeCo2.id = `containName${index}`
 
         let divNodeCo3 = document.createElement('div');
         divNodeCo3.className = 'col-sm border containImage';
 
         divNodeRow2.appendChild(divNodeCo2);
         divNodeRow2.appendChild(divNodeCo3);
+
+
+
       });
 
 
-     //Add name to DOM 
+    //Add name to DOM 
     let listAphabet = document.getElementsByClassName('listAphabet')
     let containName = document.getElementsByClassName('containName');
     // console.log(groupByAlphabetical()[listAphabet[0].textContent.charAt(0)]);
 
-    const allDogName = (arr, element1, element2, i) => {
+    const allDogName = (i) => {
 
-      arr[element1[i].textContent.charAt(0)].map(item => {
+      let selectName = groupByAlphabetical()[listAphabet[i].textContent.charAt(0)];
+      selectName.map(item => {
         let paragraph = document.createElement('p');
-        paragraph.textContent = item ? item.charAt(0).toUpperCase() + item.slice(1) :"";
-        element2[i].appendChild(paragraph)
+        paragraph.textContent = item ? item.charAt(0).toUpperCase() + item.slice(1) : "";
+        containName[i].appendChild(paragraph)
       })
     }
 
-//Add image to DOM
-let dogImagesURL = 'https://dog.ceo/api/breed/affenpinscher/images/random'; 
+    //Add image to DOM
     listAphabet = document.getElementsByClassName('listAphabet');
     let containImage = document.getElementsByClassName('containImage');
-//console.log($('#containName0').text().split(' '));
+    //console.log($('#containName0').text().split(' '));
 
-function getImageRandom(element_1,element_2,i){
+    function getImageRandom(i) {
 
-  let selectImage = groupByAlphabetical()[element_1[i].textContent.charAt(0)];
-  let numRandom = Math.floor(Math.random() * selectImage.length);
-  let fetchPromise = fetch(`https://dog.ceo/api/breed/${selectImage[numRandom]}/images/random`);
-     
- fetchPromise.then(response => response.json())
- .then(dataImage =>{
-    
-   let img = document.createElement('img')
-       img.src = Object.values(dataImage)[0];
-       img.className ='img img-thumbnail'
-       element_2[i].appendChild(img) 
- })
-}
-let i = -1;
-while (i++ < containName.length - 1) {
+      let selectImage = groupByAlphabetical()[listAphabet[i].textContent.charAt(0)];
+      let numRandom = Math.floor(Math.random() * selectImage.length);
+      let fetchPromise = fetch(`https://dog.ceo/api/breed/${selectImage[numRandom]}/images/random`);
 
-  allDogName(groupByAlphabetical(), listAphabet, containName, i); //thêm tên các giống dog
-  getImageRandom(listAphabet,containImage,i)    //thêm ảnh các giống dog và chạy random mỗi khi load
-}
+      fetchPromise.then(response => response.json())
+        .then(dataImage => {
+
+          let img = document.createElement('img')
+          img.src = Object.values(dataImage)[0];
+          img.className = 'img img-thumbnail'
+          containImage[i].appendChild(img)
+        })
+    }
+    let i = -1;
+    while (i++ < containName.length - 1) {
+
+      allDogName(i); //thêm tên các giống dog
+      getImageRandom(i)    //thêm ảnh các giống dog và chạy random mỗi khi load
+    }
 
   });
 
- // console.log($('#containName0').text());
-  
-   
 
 
-  
-  
-  
-  
-  
-  
+
+
+
+
+
+
+
